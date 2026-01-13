a6d1cf9866d33b5ade1b11882076ebddd77829f3
naila-ady <164941279+naila-ady@users.noreply.github.com>
2026-01-13 13:18:55 +0500

    .

--- backend/app.py
+++
@@ -1,6 +0,0 @@
-# This file is required by Hugging Face Spaces to run the application
-import os
-from src.main import app
-
-# The application object is what Hugging Face Spaces will look for
-# Make sure it's named 'app' and located in the root of your backend
